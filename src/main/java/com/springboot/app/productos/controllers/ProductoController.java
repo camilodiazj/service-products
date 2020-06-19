8499a73af6a66c8f7453f4371d9b5feea98faa0f
camilo.diaz <camilo.diaz@sophossolutions.com>
2020-06-18 21:20:37 -0500

    updates added

--- src/main/java/com/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/springboot/app/productos/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import com.springboot.app.productos.models.service.IProductoService;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class ProductoController {
 
   private final IProductoService iProductoService;
@@ -31,9 +33,14 @@ public class ProductoController {
   }
 
   @GetMapping("/ver/{id}")
-  public Producto detalle(@PathVariable Long id) {
+  public Producto detalle(@PathVariable Long id) throws Exception {
     Producto producto = iProductoService.findById(id);
     producto.setPort(port);
+//    try {
+//      Thread.sleep(2000L);
+//    } catch (Exception ex) {
+//      ex.printStackTrace();
+//    }
     return producto;
   }
 }
